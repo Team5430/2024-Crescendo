93fbdd44de8e024c5814dbc19160e7a8b4839e28
frc5430c <frc5430c@gmail.com>
2024-02-10 09:59:18 -0800

    Functionality removal -RL

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,18 +9,12 @@ public class Constants {
   public static double circumferenceInInches = 6 * Math.PI;
   public static double inches = 12;
 
-  
-
   public static class CANid {
     // Talons CANid
     public static int backLeftMotor = 2;
     public static int frontLeftMotor = 4;
     public static int backRightMotor = 3;
     public static int frontRightMotor = 5;
-    public static int shooterMotor;
-    public static int pivotMotor;
-    public static int intakeMotor;
-    public static int hang_motor;
   }
 
   public static class OperatorC {
